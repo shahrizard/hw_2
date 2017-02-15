c9420b0d7f2f99a7e9349a75141a0c603c671690
shahrizard <shahrizard2014@gmail.com>
2017-02-15 15:08:52 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+    allRatings = []
+    Movie.all.each do |movie|
+        allRatings.push(movie.rating)
+    end
+    return allRatings.uniq
+    end
 end
