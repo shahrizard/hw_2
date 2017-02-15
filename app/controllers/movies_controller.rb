c9a5ae9a918b419bac58e88184afaaec2c54265f
shahrizard <shahrizard2014@gmail.com>
2017-02-15 15:07:29 -0600

    hw2_complete
    completed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,26 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
-  end
+    sort_by = params[:sort_by] || session[:sort_by]
+    session[:sort_by] = sort_by
+    
+    @title_header = 'hilite' if sort_by == 'title'
+    @release_date_header = 'hilite' if sort_by == 'release_date'
+    
+    @all_ratings = Movie.all_ratings
+    
+    if params.keys.include? "ratings"
+      @ratings = params[:ratings].keys if params[:ratings].is_a? Hash
+      @ratings = params[:ratings] if params[:ratings].is_a? Array
+    elsif session.keys.include? "ratings"
+      @ratings = session[:ratings]
+    else 
+      @ratings = @all_ratings
+    end 
+    session[:ratings] = @ratings
+    redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings]) if ! ((params.keys.include? 'sort_by') || (params.keys.include? 'ratings'))
+    @movies = Movie.where(:rating => @ratings).order(sort_by)
+  end 
 
   def new
     # default: render 'new' template
@@ -43,3 +61,22 @@ class MoviesController < ApplicationController
   end
 
 end
+
+
+
+#    if params[:sort] == 'title'
+#     @movies = Movie.order('title')
+#     @title_header = 'hilite'
+#    elsif params[:sort] == 'release_date'
+#     @movies = Movie.order('release_date ASC')
+ #    @release_date_header = 'hilite'
+  #  elsif params[:ratings]
+   #  @movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
+  #  else   
+  #  @movies = Movie.all
+  #  end
+  #  @set_ratings = params[:ratings]
+  #  if !@set_ratings
+  #    @set_ratings = Hash.new
+  #  end 
+ 
